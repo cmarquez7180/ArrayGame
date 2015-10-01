9dc468b39313fd58acf3f62ea96288a2be0f1e28
Coral Marquez <cmarquez7180@stu.neisd.net>
2015-10-01 10:36:43 -0500

    Datatypes NetBeans

---
+++ Marquez_7_datatypes.java
@@ -0,0 +1,110 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package marquez_7_datatypes;
+//space for comments
+//used to explain code
+//code expectation
+//in D22
+    
+
+    
+/**
+ *
+ * @author aflynn
+ */
+public class Marquez_7_datatypes {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    
+//    int i = 1;
+//    float a = 2.5f;
+//    double b = 3.87;
+//    short c = 16;
+//    char d1 = ';';
+//    char d2 = 'y';
+//    char d3 = 42;
+//    char d4 = 145;
+//    String e = d4+"";
+//    String e1 = "amanda is the best";
+//    boolean f = true;
+//    boolean g = i % 2 == 0;
+    
+    /*
+    here we will look at some mathematical operations
+    */
+    // + this adds two numbers and more
+    // - this subtracts two numbers and more
+    // * this multiplies two numbers and more
+    // / this divides two numbers and more
+    // % this modulo. Show you the remainder
+    
+//    System.out.println(i + " an integer");
+//    System.out.println(a + " a float");
+//    System.out.println(b + " a float");
+//    System.out.println(c + " a double");
+//    System.out.println(d1 + " a char");
+//    System.out.println(d2 + " a char");
+//    System.out.println(d3 + " a char");
+//    System.out.println(e + " a string");
+//    System.out.println(f + " a boolean");
+    //for(i = -123;i < 123; i++){
+        //System.out.println((char) i);
+//        System.out.println(i % 3 == 0);
+    //}
+    /* % checks for remainder 
+      i % 2 checks if a number is
+      even or odd */
+    /* i % 3 checks to see which 
+        numbers are divisble by 3*/
+    
+    // a number value for char
+    //maps to a character
+//    i = 7;
+//    a = 0;
+//    e= " ninja ";
+//    System.out.println(i + a + " an integer /" 
+//                    + "plus a float");
+    // upcast to float is automatic
+    // i.e. 7 + 0.0 = 7.0
+//       i = 22; 
+//       a = 0;
+//       e = "ninja";
+//       System.out.println(e + i + " an /"
+//                 + "a string plus an integer"
+//       );
+       /* adding an int and a string 
+       concatenates (combines) the int
+       and string as a string*/
+//       a = 2.5f;
+//       b = 3.2;
+//      for (i = 0; i < 500; i++) {
+//          /* if is a conditional flow statement (flow)
+//          statement. If a condition is true the following
+//          command will ne executed */
+//         if (i % 2 == 0) {
+//             System.out.println(i + " is even");
+//         }else{
+//             System.out.println(i + " is odd");
+//         }
+//      } 
+    
+        
+          
+    
+    
+   // System.out.println( i + " is prime");
+       //    } else {
+       // System.out.println( i + "is not prime");
+        }
+    }
+       
+    
+    
+
