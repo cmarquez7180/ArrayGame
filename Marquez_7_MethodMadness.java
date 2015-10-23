95051404b88452e7e10def33509213e0d4198080
Coral Marquez <cmarquez7180@stu.neisd.net>
2015-10-23 10:34:06 -0500

    Final Method Madness

--- Marquez_7_MethodMadness.java
+++ Marquez_7_MethodMadness.java
@@ -6,7 +6,6 @@ import javafx.scene.Scene;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.paint.Color;
-import javafx.scene.shape.ArcType;
 import javafx.stage.Stage;
 
 public class Marquez_7_MethodMadness extends Application {
@@ -14,198 +13,97 @@ public class Marquez_7_MethodMadness extends Application {
     public static void main(String[] args) {
         launch(args);
     }
-    
 
     @Override
-    
+
     public void start(Stage primaryStage) {
         primaryStage.setTitle("Drawing Operations Test");
         Group root = new Group();
-        Canvas canvas = new Canvas(300, 300);
-
+        Canvas canvas = new Canvas(835, 500);
         GraphicsContext gc = canvas.getGraphicsContext2D();
-        //drawSomeShape(gc,47,52,84);
-        drawLotsOfCircles(gc, 900);
-        drawLotsOfRect(gc, 50);
-        drawLotsOfOvals(gc,900);
-        
+        int a = 0;
+        int b = 205;
+        int c = 500;
+        for (int i = 0; i < 1000; i++) {
+            if (i == 0) {
+                drawShape0(gc, a, b, c);
+            } else if (i == 1) {
+                drawShape1(gc, a, b, c);
+            } else if (i == 2) {
+                drawShape2(gc, a, b, c);
+            } else if (i == 3) {
+                drawShape3(gc, a, b, c);
+            } else if (i == 4) {
+                drawShape4(gc, a, b, c);
+            } else if (i == 5) {
+                drawShape5(gc, a, b, c);
+            } else if (i == 6) {
+                drawShape6(gc, a, b, c);
+            } else if (i == 7) {
+                drawShape7(gc, a, b, c);
+            } else if (i == 8) {
+                drawShape8(gc, a, b, c);
+            } else if (i == 9) {
+                drawShape9(gc, a, b, c);
+            }
+            a = a + 50;
+            b = b + 20;
+            c = c - 50;
+
+        }
         root.getChildren().add(canvas);
         primaryStage.setScene(new Scene(root));
         primaryStage.show();
     }
-    
 
-    private void drawSomeShape(GraphicsContext gc, int x, int y, int d) {
+    private void drawShape0(GraphicsContext gc, int x, int y, int z) {
         gc.setFill(Color.AQUA);
-        gc.fillOval(x, y, d, d);
+        gc.fillRect(0, x, y, z);
     }
 
-    private void drawLotsOfCircles(GraphicsContext gc, int num) {
-        for (int i = 0; i < 300; i++) {
-
-                   
-                        if (i % 55 == 0) {
-                            gc.setFill(Color.AQUA);
-                            gc.fillOval(225, 200, 50, 50);
-                        } else {
-                            
-                                if (i % 22 == 0) {
-                                    gc.setFill(Color.HOTPINK);
-                                    gc.fillOval(175, 150, 50, 50);
-                                    }else{
-                                    
-                                        if ( isDivisiblebythree(i)) {
-                                            gc.setFill(Color.FUCHSIA);
-                                            gc.fillOval(125, 100, 50, 50);
-                                        }else{
-                                            
-                                                if (isARect(i)) {
-                                                    gc.setFill(Color.YELLOW);
-                                                    gc.fillOval(75, 50, 50, 50);
-                                                    }else{
-                                                if (isARect2(i)) {
-                                                    gc.setFill(Color.AQUAMARINE);
-                                                    gc.fillOval(50, 25, 50, 50);
-                                                    }else{
-                                                if (isARect3(i)) {
-                                                    gc.setFill(Color.CORNFLOWERBLUE);
-                                                    gc.fillOval(25, 0, 50, 50);
-                                                    }else{
-                                                
-                                                
-                                                
-                                                
-
-                                
-
-                            
-                        
-                    }
-                }
-            }
-        }
-    }
-            }
-        }
-    }
-        private void drawLotsOfOvals(GraphicsContext gc, int num) {
-        for (int i = 0; i < 1000; i++) {
-               
-                    
-                        if (i % 55 == 0) {
-                            gc.setFill(Color.AQUA);
-                            gc.fillOval(225, 250, 50, 50);
-                        } else {
-                            
-                                if (i % 22 == 0) {
-                                    gc.setFill(Color.HOTPINK);
-                                    gc.fillOval(175, 200, 50, 50);
-                                    }else{
-                                    
-                                        if ( isDivisiblebythree(i)) {
-                                            gc.setFill(Color.FUCHSIA);
-                                            gc.fillOval(125, 150, 50, 50);
-                                        }else{
-                                            
-                                                if (isARect(i)) {
-                                                    gc.setFill(Color.YELLOW);
-                                                    gc.fillOval(75, 100, 50, 50);
-                                                    }else{
-                                                if (isARect2(i)) {
-                                                    gc.setFill(Color.AQUAMARINE);
-                                                    gc.fillOval(50, 75, 50, 50);
-                                                    }else{
-                                                if (isARect3(i)) {
-                                                    gc.setFill(Color.CORNFLOWERBLUE);
-                                                    gc.fillOval(25, 50, 50, 50);
-                                                    }else{
-                                               
-                                                
-                                                
-                                                
-
-                                
-
-                            
-                        
-                    }
-                }
-            }
-        }
-    }
-            }
-        }
-    }
-private void drawLotsOfRect(GraphicsContext gc, int num) {
-        for (int i = 0; i < 1000; i++) {
-                
-                    
-                        if (i % 55 == 0) {
-                            gc.setFill(Color.AQUA);
-                            gc.fillRect(225, 225, 50, 50);
-                        } else {
-                            
-                                if (i % 22 == 0) {
-                                    gc.setFill(Color.HOTPINK);
-                                    gc.fillRect(175, 175, 50, 50);
-                                } else {
-                                    
-                                        if ( isDivisiblebythree(i)) {
-                                            gc.setFill(Color.FUCHSIA);
-                                            gc.fillRect(125, 125, 50, 50);
-                                        }else{
-                                           
-                                                if (isARect(i)) {
-                                                    gc.setFill(Color.YELLOW);
-                                                    gc.fillRect(75, 75, 50, 50);
-                                                    }else{
-                                                if (isARect2(i)) {
-                                                    gc.setFill(Color.AQUAMARINE);
-                                                    gc.fillRect(50, 50, 50, 50);
-                                                    }else{
-                                                if (isARect3(i)) {
-                                                    gc.setFill(Color.CORNFLOWERBLUE);
-                                                    gc.fillRect(25, 25, 50, 50);
-                                                    }else{
-                                                
-                                                
-                                                
-                                                
-                                    
-                               
-                            
-
-                        }
-                            }
-                        }
-                    }
-                }
-            } 
-        }
+    private void drawShape1(GraphicsContext gc, int x, int y, int z) {
+        gc.setFill(Color.CORNFLOWERBLUE);
+        gc.fillRect(50, x, y, z);
     }
 
-    
-    static boolean isDivisiblebyfive(int i) {
-        return (i % 5 == 0);
+    private void drawShape2(GraphicsContext gc, int x, int y, int z) {
+        gc.setFill(Color.AQUAMARINE);
+        gc.fillRect(100, x, y, z);
     }
 
-    static boolean isDivisiblebythree(int i) {
-        return (i % 3 == 0);
+    private void drawShape3(GraphicsContext gc, int x, int y, int z) {
+        gc.setFill(Color.YELLOW);
+        gc.fillRect(150, x, y, z);
     }
-    static boolean isDivisiblebytwo(int i) {
-        return (i % 2 == 0);
+
+    private void drawShape4(GraphicsContext gc, int x, int y, int z) {
+        gc.setFill(Color.FUCHSIA);
+        gc.fillRect(200, x, y, z);
     }
-    static boolean isARect(int i){
-        return (i % 7 == 0);
+
+    private void drawShape5(GraphicsContext gc, int x, int y, int z) {
+        gc.setFill(Color.HOTPINK);
+        gc.fillRect(250, x, y, z);
     }
-    static boolean isARect2(int i){
-        return (i % 9 == 0);
+
+    private void drawShape6(GraphicsContext gc, int x, int y, int z) {
+        gc.setFill(Color.AQUA);
+        gc.fillRect(300, x, y, z);
     }
-    static boolean isARect3(int i){
-        return (i % 11 == 0);
+
+    private void drawShape7(GraphicsContext gc, int x, int y, int z) {
+        gc.setFill(Color.CORNFLOWERBLUE);
+        gc.fillRect(350, x, y, z);
     }
-    static boolean isARect4(int i){
-        return (i % 13 == 0);
+
+    private void drawShape8(GraphicsContext gc, int x, int y, int z) {
+        gc.setFill(Color.AQUAMARINE);
+        gc.fillRect(400, x, y, z);
+    }
+
+    private void drawShape9(GraphicsContext gc, int x, int y, int z) {
+        gc.setFill(Color.YELLOW);
+        gc.fillRect(450, x, y, z);
     }
 
 }
